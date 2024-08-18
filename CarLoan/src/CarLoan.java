b4583a50e14202f42e128f3dbf49d0a1ef9bbbfa
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-08-18 14:37:48 +0100

    create comments

--- CarLoan/src/CarLoan.java
+++ CarLoan/src/CarLoan.java
@@ -1,22 +1,23 @@
 public class CarLoan {
     public static void main(String[] args) throws Exception {
-        int carLoan = 1000;
-        int loanLength = 3; // loan length of 3 years
-        int interestRate = 5; // interest rate of 5%
-        int downPayment = 2000; // down payment by a user
-
-    if (loanLength <= 0 || interestRate <= 0) {
-          System.out.println("Error! You must take out a valid car loan.");
-    } else if (downPayment >=carLoan)  {
-        System.out.println("The car can be paid in full");
-    } else {
-        int remainingBalance = carLoan - downPayment;
-        int months = loanLength * 12;
-        int monthlyBalance = remainingBalance / months;
-        int interest = monthlyBalance * interestRate / 100;
-        int monthlyPayment = monthlyBalance + interest;
+            int carLoan = 10000;
+            int loanLength = 3; // loan length of 3 years
+            int interestRate = 5; // interest rate of 5%
+            int downPayment = 2000; // down payment by a user
     
-        System.out.println(monthlyPayment);
-    }
-    }
+        if (loanLength <= 0 || interestRate <= 0) {
+              System.out.println("Error! You must take out a valid car loan."); // when input is not valid
+        } else if (downPayment >= carLoan)  {
+            System.out.println("The car can be paid in full"); 
+        } else {
+            int remainingBalance = carLoan - downPayment;
+            int months = loanLength * 12;
+            int monthlyBalance = remainingBalance / months;
+            int interest = (monthlyBalance * interestRate)/ 100; // calculate interest
+            int monthlyPayment = monthlyBalance + interest;
+
+            System.out.println(monthlyPayment);
+        }
+        }
 }
+
